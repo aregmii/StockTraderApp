ebd8a772c1d1436c180f8d9fd07da329f9ee1bf8
Amish Regmi <amishregmi@Amishs-MBP.attlocal.net>
2024-01-30 22:26:36 -0600

    add unit tests

---
+++ src/test/java/org/example/stocktrader/facades/StockDataFacadeTest.java
@@ -0,0 +1,31 @@
+package org.example.stocktrader.facades;
+
+import org.example.stocktrader.listener.StockDataStreamListener;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StockDataFacadeTest {
+
+    private StockDataFacade stockDataFacade;
+    private StockDataStreamListener listener = Mockito.mock(StockDataStreamListener.class);
+
+    /**
+     * This test case tests the startStreaming method of StockDataFacade class.
+     */
+    @Test
+    public void testStartStreaming() {
+        // Initialize the object to be tested with mock dependencies
+        stockDataFacade = new StockDataFacade(listener);
+        List<String> symbols = Arrays.asList("AAPL", "GOOGL", "MSFT");
+
+        // Call the method to be tested
+        stockDataFacade.startStreaming(symbols);
+
+        // Verify the interaction with the listener
+        Mockito.verify(listener, Mockito.times(1)).connectAndSubscribe(symbols);
+    }
+}
---
+++ src/test/java/org/example/stocktrader/listener/StockDataStreamListenerTest.java
@@ -0,0 +1,95 @@
+package org.example.stocktrader.listener;
+
+import net.jacobpeterson.alpaca.AlpacaAPI;
+import net.jacobpeterson.alpaca.model.endpoint.clock.Clock;
+import net.jacobpeterson.alpaca.model.endpoint.marketdata.common.realtime.enums.MarketDataMessageType;
+import net.jacobpeterson.alpaca.model.endpoint.marketdata.stock.realtime.bar.StockBarMessage;
+import net.jacobpeterson.alpaca.rest.endpoint.clock.ClockEndpoint;
+import org.example.stocktrader.calculator.QueueMovingAverageCalculator;
+import org.example.stocktrader.calculator.QueueMovingAverageCalculatorRegistry;
+import org.example.stocktrader.publisher.StreamInputMessagePublisher;
+import org.example.stocktrader.publisher.impl.BarStreamInputMessagePublisher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class StockDataStreamListenerTest {
+
+    @Mock
+    private AlpacaAPI alpacaAPI;
+
+    @Mock
+    private Executor executorService;
+
+    @Mock
+    private List<StreamInputMessagePublisher<?>> handlers;
+
+    @Mock
+    private BarStreamInputMessagePublisher barStreamInputMessageHandler;
+
+    @Mock
+    private Clock clock;
+
+    @Mock
+    private ClockEndpoint clockEndpoint;
+
+    @Mock
+    QueueMovingAverageCalculator queueMovingAverageCalculator;
+
+    private StockDataStreamListener stockDataStreamListener;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        stockDataStreamListener = new StockDataStreamListener(
+                alpacaAPI,
+                5,
+                executorService
+        );
+    }
+
+    @Test
+    @DisplayName("Testing StockDataStreamListener connectAndSubscribe method when the market is open")
+    public void testConnectAndSubscribeWhenMarketOpen() throws Exception {
+        mockClock(true);
+
+        // Preparing the Singleton for the test
+        QueueMovingAverageCalculatorRegistry.validators.put(MarketDataMessageType.BAR, queueMovingAverageCalculator);
+
+        stockDataStreamListener.connectAndSubscribe(Collections.singletonList("APPL"));
+
+        verify(clock, times(1)).getIsOpen();
+        verify(barStreamInputMessageHandler, never()).handleStreamInput(any(StockBarMessage.class), any(Instant.class));
+    }
+
+    @Test
+    @DisplayName("Testing StockDataStreamListener connectAndSubscribe method when the market is closed")
+    public void testConnectAndSubscribeWhenMarketClosed() throws Exception {
+        mockClock(false);
+
+        // Preparing the Singleton for the test
+        QueueMovingAverageCalculatorRegistry.validators.put(MarketDataMessageType.BAR, queueMovingAverageCalculator);
+
+        stockDataStreamListener.connectAndSubscribe(Collections.singletonList("APPL"));
+
+        verify(clock, times(1)).getIsOpen();
+        //verify(barStreamInputMessageHandler, atLeastOnce()).handleStreamInput(any(StockBarMessage.class), any(Instant.class));
+    }
+
+    private void mockClock(boolean isOpen) throws Exception {
+        when(clock.getIsOpen()).thenReturn(isOpen);
+        when(alpacaAPI.clock()).thenReturn(clockEndpoint);
+        when(clockEndpoint.get()).thenReturn(clock);
+    }
+}

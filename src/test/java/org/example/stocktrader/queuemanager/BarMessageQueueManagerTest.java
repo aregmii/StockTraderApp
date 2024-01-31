2fa8d954c9ae032c52e35b90c2fd74d16ee69bb0
Amish Regmi <amishregmi@Amishs-MBP.attlocal.net>
2024-01-30 22:26:36 -0600

    add unit tests

--- src/test/java/org/example/stocktrader/queuemanager/BarMessageQueueManagerTest.java
+++ src/test/java/org/example/stocktrader/queuemanager/BarMessageQueueManagerTest.java
@@ -7,7 +7,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import net.jacobpeterson.alpaca.model.endpoint.marketdata.common.realtime.bar.BarMessage;
 
@@ -28,16 +28,19 @@ public class BarMessageQueueManagerTest {
     @InjectMocks
     private BarMessageQueueManager barMessageQueueManager;
 
+    private BarMessage barMessage;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         barMessagesInMemoryMapExcludingTarget = new HashMap<>();
         barMessageInMemoryMapTargetStock = new HashMap<>();
+        barMessage = new BarMessage();
+
     }
 
     @Test
     public void testPublish() {
-        BarMessage barMessage = new BarMessage();
         barMessage.setSymbol("ABC");
         barMessage.setTimestamp(ZonedDateTime.now());
         
@@ -51,4 +54,37 @@ public class BarMessageQueueManagerTest {
     }
 
     // Additional test cases should be created to test other method functionalities
+    @Test
+    public void testExecute_withTargetStock() {
+        Map<String, Queue<BarMessage>> testMapTarget = new HashMap<>();
+        Map<String, Queue<BarMessage>> testMapExcludingTarget = new HashMap<>();
+        BarMessageQueueManager manager = new BarMessageQueueManager(testMapExcludingTarget, testMapTarget);
+        BarMessage testBarMessage = createMockBarMessage("AAPL");
+
+        when(testBarMessage.getSymbol()).thenReturn("AAPL");
+        manager.execute(testBarMessage);
+
+        verify(barMessage, times(1)).getSymbol();
+    }
+
+    @Test
+    public void testExecute_withOtherStock() {
+        Map<String, Queue<BarMessage>> testMapTarget = new HashMap<>();
+        Map<String, Queue<BarMessage>> testMapExcludingTarget = new HashMap<>();
+
+        BarMessageQueueManager manager = new BarMessageQueueManager(testMapExcludingTarget, testMapTarget);
+        BarMessage testBarMessage = createMockBarMessage("TSLA");
+
+        when(testBarMessage.getSymbol()).thenReturn("TSLA");
+        manager.execute(testBarMessage);
+
+        verify(barMessage, times(1)).getSymbol();
+    }
+
+    private BarMessage createMockBarMessage(String symbol){
+        BarMessage barMessage = mock(BarMessage.class);
+        when(barMessage.getTimestamp()).thenReturn(ZonedDateTime.now());
+        when(barMessage.getSymbol()).thenReturn(symbol);
+        return barMessage;
+    }
 }
